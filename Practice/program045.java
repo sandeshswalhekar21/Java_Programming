8496d52674daeccd049ffd0a3a53f59b6271084f
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-08-02 23:56:18 +0530

    added new program

---
+++ Practice/program045.java
@@ -0,0 +1,56 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+class program147
+{
+    
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+       
+ 
+        System.out.println("Enter the number of Elements:");
+        int iLength = sobj.nextInt();
+
+        
+        int Brr[]= new int[iLength];
+
+        System.out.println("Enter the Elements:");
+        int iCnt=0;
+        for(iCnt=0;iCnt<Brr.length;iCnt++)
+        {
+            Brr[iCnt]=sobj.nextInt();
+        }
+        System.out.println("Elements before function the Array are :");
+         for(iCnt=0;iCnt<Brr.length;iCnt++)
+        {
+           System.out.println(Brr[iCnt]);
+        }
+
+        ArrayX aobj = new ArrayX();
+        aobj.Update(Brr);                               //callby reference 
+
+         System.out.println("Elements After function are Array are :");
+         for(iCnt=0;iCnt<Brr.length;iCnt++)
+        {
+           System.out.println(Brr[iCnt]);
+        }                      
+        
+        Brr = null;
+        System.gc();   
+
+    }   
+}
+public class ArrayX
+{
+     public  void Update(int Arr[])
+    {
+        int iCnt=0;
+        for(iCnt=0;iCnt<Arr.length;iCnt++)
+        {
+           Arr[iCnt]++;
+        }
+    }
+
+} 
