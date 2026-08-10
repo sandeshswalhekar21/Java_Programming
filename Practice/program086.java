81a891d50131c465032d4b66857137357e1ccfe0
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-08-10 22:05:23 +0530

    added new program

---
+++ Practice/program086.java
@@ -0,0 +1,60 @@
+/*
+   iRow = 4
+   iCol=4
+   
+  # # # $
+  # # $ #
+  # $ # #
+  $ # # #
+  
+   
+
+*/
+
+import java.util.*;
+
+
+class program210
+{
+    public static void main(String[] A) 
+    {
+        int iValue1=0;
+        int iValue2=0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of Rows:");
+        iValue1=sobj.nextInt();
+
+        System.out.println("Enter the number of Column:");
+        iValue2=sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1,iValue2);       
+    }
+}
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int i=0,j=0;
+        
+       
+        for(i=iRow;i >= 1; i--)
+        {
+            for(j=1;j<=iCol;j++)
+            {
+               if(i==j)
+               {
+                System.out.print("$\t");
+               }else{
+                System.out.print("#\t");
+               }
+            }
+           System.out.println();
+                
+        }
+           
+    }  
+}
