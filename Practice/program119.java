80177fd00b8bc5089083aa9aede4e9ed67db1889
sandeshswalhekar21 <sandeshswalhekar@gmail.com>
2026-09-06 23:36:24 +0530

    added new program

---
+++ Practice/program119.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class program269
+{
+    public static void main(String A[])
+     {
+        Scanner sobj = new Scanner(System.in);
+        String data = null;
+
+        StringX strobj = new StringX();
+
+
+        System.out.println("Enter String:");
+        data = sobj.nextLine();
+
+
+        strobj.Display(data);
+        
+    }
+}
+class StringX
+{
+    public void Display(String str)
+    {
+        System.out.println("Recived String is:"+str);
+
+    }
+}
